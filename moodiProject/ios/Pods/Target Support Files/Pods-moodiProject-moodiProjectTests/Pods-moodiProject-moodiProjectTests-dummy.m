25eaece42a3d34f72b9951581ec9a36d62dd1887
ovdncids <ovdncids@nate.com>
2021-04-01 22:36:29 +0900

    pods

---
+++ moodiProject/ios/Pods/Target Support Files/Pods-moodiProject-moodiProjectTests/Pods-moodiProject-moodiProjectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_moodiProject_moodiProjectTests : NSObject
+@end
+@implementation PodsDummy_Pods_moodiProject_moodiProjectTests
+@end
---
+++ moodiProject/ios/Pods/Target Support Files/Pods-moodiProject/Pods-moodiProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_moodiProject : NSObject
+@end
+@implementation PodsDummy_Pods_moodiProject
+@end
